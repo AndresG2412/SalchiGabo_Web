f16fcd0e6265b8ac5c20f399dad846f45ec9e774
Andres Gaviria <cgaviria930@gmail.com>
2025-01-06 18:18:44 -0500

    add fondo canvas

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Opulento  } from "uvcanvas"
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <>
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        zIndex: -1,
+        opacity: 0.2,
+      }}
+    >
+      <Opulento />
+    </div>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </>
+);
