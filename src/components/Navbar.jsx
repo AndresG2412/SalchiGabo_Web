16af2b93d5a3816cc149e9bdf42b22a8fc3c17b6
Andres Gaviria <cgaviria930@gmail.com>
2025-01-08 23:59:41 -0500

    cambios responsible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import perfil from '../images/assets/perfil.png'
 
 export default function Navbar() {
     return(
-        <div className='hidden min-w-[8%] md:flex flex-col items-center justify-center md:h-screen border-l-4 border-black right-0' style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-            <button><img src={car} alt="btn_navbar" className='w-14 my-8' /></button>
-            <button><img src={perfil} alt="btn_navbar" className='w-14 my-8' /></button>
-            <button><img src={menu} alt="btn_navbar" className='w-14 my-8' /></button>
+        <div className='hidden md:min-w-[8%] lg:min-w-[6%] md:flex flex-col items-center justify-center md:h-screen border-l-4 border-black right-0' style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+            <button><img src={car} alt="btn_navbar" className='lg:w-14 md:w-10 lg:my-8 md:my-5'/></button>
+            <button><img src={perfil} alt="btn_navbar" className='lg:w-14 md:w-10 lg:my-8 md:my-5'/></button>
+            <button><img src={menu} alt="btn_navbar" className='lg:w-14 md:w-10 lg:my-8 md:my-5'/></button>
         </div>
     )
 }
