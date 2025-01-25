cfdda7f9db7e5810f157d6f111a24fdb96812642
Andres Gaviria <cgaviria930@gmail.com>
2025-01-25 11:43:44 -0500

    cambio iconos botones cartas

--- src/components/New/CardV2.jsx
+++ src/components/New/CardV2.jsx
@@ -13,8 +13,8 @@ export default function CardV2({ruta, titulo, descripcion, precio}) {
                     <p className="font-nunito font-bold text-2xl text-white my-6 md:my-4">$ {precio} pesos</p>
                 </div>
                 <div className="flex justify-center gap-4">
-                    <button className="flex items-center justify-center gap-2 font-bold py-2 shadow-2xl bg-[#cdf9dd] text-center rounded-[15px] w-36 hover:scale-110 transition-transform"><img src={add} alt="add" className="w-[30px]"/>ORDENAR</button>
-                    <button className="flex items-center justify-center gap-2 font-bold py-2 shadow-2xl bg-[#f9f7cd] text-center rounded-[15px] w-36 hover:scale-110 transition-transform"><img src={send} alt="add" className="w-[25px]"/>AÑADIR</button>
+                    <button className="flex items-center justify-center gap-2 font-bold py-2 shadow-2xl bg-[#cdf9dd] text-center rounded-[15px] w-36 hover:scale-110 transition-transform"><img src={send} alt="add" className="w-[30px]"/>ORDENAR</button>
+                    <button className="flex items-center justify-center gap-2 font-bold py-2 shadow-2xl bg-[#f9f7cd] text-center rounded-[15px] w-36 hover:scale-110 transition-transform"><img src={add} alt="add" className="w-[25px]"/>AÑADIR</button>
                 </div>
             </div>
         </div>
