877d20397537fe59655c6afcad9b5e2c485b6346
Andres Gaviria <cgaviria930@gmail.com>
2025-01-26 14:06:46 -0500

    cambios NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,8 @@
 export default function NotFound(){
     return(
-        <div className="h-screen flex flex-col gap-y-6 items-center justify-center fomt-bold tracking-wider">
-            <p className="text-3xl font-nunito text-center text-white">Pagina no encontrada!</p>
-            <p className="text-3xl font-nunito text-center text-white"> Vuelve por donde llegaste</p>
+        <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl gap-y-6 text-white tracking-wider font-bold">
+            <p className="border-b-2 border-white pb-1">Esta pagina no existe!</p>
+            <p className="border-b-2 border-white pb-1">Vuelve por donde llegaste</p>
         </div>
     )
 }
