dd415cd3e6344daea4fc9d841a22004f6afaf1bf
Andres Gaviria <cgaviria930@gmail.com>
2025-01-26 14:16:10 -0500

    cambios NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
+import Navbar from "../components/Navbar"
+
 export default function NotFound(){
     return(
-        <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl gap-y-6 text-white tracking-wider font-bold">
-            <p className="border-b-2 border-white pb-1">Esta pagina no existe!</p>
-            <p className="border-b-2 border-white pb-1">Vuelve por donde llegaste</p>
+        <div className="w-screen h-screen flex flex-col justify-center items-center gap-y-6 ">
+            <p className="border-b-2 text-2xl border-white pb-1 text-white tracking-wider font-bold">Esta pagina no existe!</p>
+            <p className="border-b-2 text-2xl border-white pb-1 text-white tracking-wider font-bold">Vuelve por donde llegaste</p>
+            <Navbar />
         </div>
+
     )
 }
