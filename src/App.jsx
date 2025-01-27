abad5edce0fa97835f5cbbe825209cc02f889df4
Andres Gaviria <cgaviria930@gmail.com>
2025-01-27 17:17:08 -0500

    nuevas paginas/rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router";
 
 //rutas importadas
 import Home from "./pages/Home.jsx";
-import NotFound from "./pages/NotFound.jsx";
 import Carrito from "./pages/Carrito.jsx"
+import Pedido from "./pages/Pedido.jsx"
+import Comments from "./pages/Pedido.jsx"
+import NotFound from "./pages/NotFound.jsx";
 
 export default function App() {
     return(
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/carrito" element={<Carrito />} />
+            <Route path="/comentarios" element={<Comments />} />
+            <Route path="/pedido" element={<Pedido />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     )
