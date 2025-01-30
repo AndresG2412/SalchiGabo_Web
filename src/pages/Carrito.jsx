111e04d4bd83119959d19ebba4d84886b5b03f6d
Andres Gaviria <cgaviria930@gmail.com>
2025-01-30 17:41:25 -0500

    front end carrito mejorado

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -10,22 +10,29 @@ export default function Carrito() {
   return (
     <>
       <LogoSM />
-      <Navbar />
 
       <div className="p-4">
-        <h1 className="text-2xl font-bold">Carrito de Compras</h1>
-        {cart.length === 0 ? (
-          <p>No has añadido nada aún.</p>
-        ) : (
-          <ul>
-            {cart.map((item, index) => (
-              <li key={index} className="border-b py-2">
-                {item.titulo} - ${item.precio}
-              </li>
-            ))}
-          </ul>
-        )}
+        <h1 className="text-4xl font-bold text-white text-center mb-8">Carrito de Compras</h1>
+        <div className="bg-white md:w-2/5 rounded-lg text-xl font-semibold font-nunito px-10 py-6">
+          {cart.length === 0 ? (
+            <p className="">No has añadido nada aún.</p>
+          ) : (
+            <div>
+              <ul className="border-b-2 border-black">
+                {cart.map((item, index) => (
+                  <li key={index} className="py-2 border-t-2 border-black pl-3">
+                    {item.titulo} - ${item.precio}
+                  </li>
+                ))}
+              </ul>
+              <p className="pt-2 pl-3">total:</p>
+            </div>
+          )}
+        </div>
       </div>
+
+      
+      <Navbar />
     </>
   );
 }
